8287e92fd5a205177ad262b7efab1e09b410232f
Hans Jakob Håland <hansjakob.haland99@gmail.com>
2024-03-23 17:01:14 +0100

    Removes unnecessary code and comments

--- training.py
+++ training.py
@@ -1,11 +1,8 @@
 import preprocessing as pp
-import encodings as one_hot
-import numpy as np
 from models import FeedforwardAminoToStructure, FeedforwardStructureToStructure
 import torch.nn as nn
 import torch.optim as optim
 import torch
-
 
 def train_feedforward_amino_to_structure_model(inputs, targets, learning_rate=0.001, num_epochs=100) -> FeedforwardAminoToStructure:
     inputs = torch.Tensor(inputs)
@@ -29,29 +26,8 @@ def train_feedforward_amino_to_structure_model(inputs, targets, learning_rate=0.
             print(f"Loss in epoch {i+1}: {loss.item()}")
     
     return model
+   
     
-def get_feedforward_amino_to_structure_predictions():
-    pass
-
-
-# NOTE: This is relevant when doing inference with both FF models
-# def get_feedforward_structure_to_structure_model_inputs(amino_to_structure_model, 
-#                                                         window_size,
-#                                                         amino_acid_one_hot,
-#                                                         secondary_structure_one_hot,
-#                                                         train_data_file):
-#     sequence_pairs = pp.load_sequences_from_file(train_data_file)
-#     amino_sequence = [sequence[0] for sequence in sequence_pairs]
-#     amino_sequence_one_hots = [pp.get_one_hot_encoding_of_sequence(sequence, amino_acid_one_hot) for sequence in amino_sequence]
-
-#     structure_windows = []
-#     for amino_sequence in amino_sequence_one_hots:
-#         amino_window = pp.get_sequence_windows(amino_sequence, window_size, amino_acid_one_hot)
-#         amino_window = np.asarray(amino_window)
-#         structure_classification_probabilities = get_feedforward_amino_to_structure_predictions(amino_to_structure_model,
-#                                                                                                 amino_window)
-
-
 def train_feedforward_structure_to_structure_model(inputs, targets, learning_rate=0.001, num_epochs=100) -> FeedforwardStructureToStructure:
     inputs = torch.Tensor(inputs)
     targets = torch.Tensor(targets)
@@ -76,9 +52,9 @@ def train_feedforward_structure_to_structure_model(inputs, targets, learning_rat
     return model
 
 
-
 def save_trained_model(model, save_file_path):
     torch.save(model, save_file_path)
+
 
 if __name__ == "__main__":
     train_data_file = "data/protein-secondary-structure.train"
