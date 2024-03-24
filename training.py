db2213758c4390b897775bcec180e0eb3cef1ad3
Hans Jakob Håland <hansjakob.haland99@gmail.com>
2024-03-24 18:51:15 +0100

    Adds option for saving trained models

--- training.py
+++ training.py
@@ -58,13 +58,22 @@ def save_trained_model(model, save_file_path):
 
 if __name__ == "__main__":
     train_data_file = "data/protein-secondary-structure.train"
-    test_data_file = "data/protein-secondary-structure.test"     
+    test_data_file = "data/protein-secondary-structure.test"   
+    
+    save_file_path_amino = "pretrained/feedforward_amino_to_structure.pt"  
+    save_file_path_structure = "pretrained/feedforward_structure_to_structure.pt"
     
     X_train_a_to_s, y_train_a_to_s, X_test_a_to_s, y_test_a_to_s = pp.get_feedforward_amino_to_structure_data_sets(train_data_file, test_data_file)
     X_train_s_to_s, y_train_s_to_s, X_test_s_to_s, y_test_s_to_s = pp.get_feedforward_structure_to_structure_data_sets(train_data_file, test_data_file)
     
-    amino_to_structure_model = train_feedforward_amino_to_structure_model(X_train_a_to_s, y_train_a_to_s)
-    structure_to_structure_model = train_feedforward_structure_to_structure_model(X_train_s_to_s, y_train_s_to_s)
+    num_epochs = 1000
+    learning_rate = 0.001
+    
+    amino_to_structure_model = train_feedforward_amino_to_structure_model(X_train_a_to_s, y_train_a_to_s, learning_rate, num_epochs)
+    structure_to_structure_model = train_feedforward_structure_to_structure_model(X_train_s_to_s, y_train_s_to_s, learning_rate, num_epochs)
+    
+    save_trained_model(amino_to_structure_model, save_file_path_amino)
+    save_trained_model(structure_to_structure_model, save_file_path_structure)
     
     print(amino_to_structure_model)
     print(structure_to_structure_model)
