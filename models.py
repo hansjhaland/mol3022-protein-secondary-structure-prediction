6ab0557199b303797d0447be16b883489f3e7a85
Hans Jakob Håland <hansjakob.haland99@gmail.com>
2024-03-26 11:10:51 +0100

    Removes LSTM model

--- models.py
+++ models.py
@@ -68,25 +68,56 @@ class FeedforwardStructureToStructure(nn.Module):
         return x
     
     
-class LSTM(nn.Module):
-    
-    def __init__(self, input_size: int, num_classes: int, hidden_size:int):
+class CNN2D(nn.Module):
+    def __init__(self, num_classes: int):
         super().__init__()
         
-        self.lstm = nn.LSTM(input_size, hidden_size)
+        self.conv2d = nn.Conv2d(1, 16, 5, padding="same")
+        self.avg_pool = nn.AvgPool2d(4,4)
         
-        self.hidden1 = nn.Linear(hidden_size, 32)
-        self.output = nn.Linear(32, num_classes)
+        self.hidden1 = nn.Linear(240, 240)
+        
+        self.output = nn.Linear(240, num_classes)
         
         self.relu = nn.ReLU()
-        self.tanh = nn.Tanh()
-        self.softmax = nn.Softmax()
+        self.softmax = nn.Softmax(dim=0)
         
     def forward(self, x):
-        x, _ = self.lstm(x)
-        x = self.tanh(x)
+        x = self.conv2d(x)
+        x = self.avg_pool(x)
+        x = torch.flatten(x, start_dim=1)
         x = self.relu(self.hidden1(x))
         x = self.output(x)
         x = self.softmax(x)
-        return x[:, -1]
+        # print(x)
+        return x
+    
+    
+class CNN1D(nn.Module):
+    def __init__(self, num_classes: int):
+        super().__init__()
+
+        self.conv1d = nn.Conv1d(1, 1, 7, padding="same")
+        # self.conv1d2 = nn.Conv1d(16, 1, 13, padding="same")
+        self.avg_pool = nn.AvgPool1d(5,5)
+    
+        self.hidden1 = nn.Linear(52, 32)
+        # self.hidden2 = nn.Linear(32, 32)
+        self.output = nn.Linear(32, num_classes)
+        
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)
+        self.sigmoid = nn.Sigmoid()
+    
+    def forward(self, x):
+        x = self.relu(self.conv1d(x))
+        x = self.avg_pool(x)
+        # x = self.relu(self.conv1d2(x))
+        # x = self.avg_pool(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.relu(self.hidden1(x))
+        # x = self.relu(self.hidden2(x))
+        x = self.output(x)
+        x = self.softmax(x)
+        return x
     
